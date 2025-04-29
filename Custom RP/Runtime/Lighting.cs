e67e31fc8debd056376675ab190eb898ee7d3234
DarkSleeper <793613936@qq.com>
2025-04-29 17:52:24 +0800

    4.1 Rendering Shadows

--- Custom RP/Runtime/Lighting.cs
+++ Custom RP/Runtime/Lighting.cs
@@ -26,13 +26,24 @@ public class Lighting
 
     CullingResults cullingResults;
 
-    public void Setup(ScriptableRenderContext context, CullingResults cullingResults) {
+    Shadows shadows = new Shadows();
+
+    public void Setup(
+        ScriptableRenderContext context, CullingResults cullingResults,
+        ShadowSettings shadowSettings
+    ) {
         this.cullingResults = cullingResults;
         buffer.BeginSample(bufferName);
+        shadows.Setup(context, cullingResults, shadowSettings);
         SetupLights();
+        shadows.Render();
         buffer.EndSample(bufferName);
         context.ExecuteCommandBuffer(buffer);
         buffer.Clear();
+    }
+
+    public void Cleanup() {
+        shadows.Cleanup();
     }
 
     void SetupLights() {
@@ -55,5 +66,6 @@ public class Lighting
     void SetupDirectionalLight(int index, ref VisibleLight visibleLight) {
         dirLightColors[index] = visibleLight.finalColor;
         dirLightDirections[index] = -visibleLight.localToWorldMatrix.GetColumn(2);
+        shadows.ReserveDirectionalShadows(visibleLight.light, index);
     }
 }
