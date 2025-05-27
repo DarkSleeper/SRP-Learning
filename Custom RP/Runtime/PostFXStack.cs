00a09fb02259cf1c6c75330e52835746d50ef48c
Fu Renhong <793613936@qq.com>
2025-05-27 20:01:29 +0800

    11.2 Bloom

--- Custom RP/Runtime/PostFXStack.cs
+++ Custom RP/Runtime/PostFXStack.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -5,6 +6,10 @@ public partial class PostFXStack
 {
     enum Pass
     {
+        BloomPrefilter,
+        BloomCombine,
+        BloomHorizontal,
+        BloomVertical,
         Copy
     }
 
@@ -20,6 +25,27 @@ public partial class PostFXStack
     Camera camera;
 
     PostFXSettings settings;
+
+    int
+        bloomBicubicUpsamplingId = Shader.PropertyToID("_BloomBicubicUpsampling"),
+        bloomPrefilterId = Shader.PropertyToID("_BloomPrefilter"),
+        bloomThresholdId = Shader.PropertyToID("_BloomThreshold"),
+        bloomIntensityId = Shader.PropertyToID("_BloomIntensity"),
+        fxSourceId = Shader.PropertyToID("_PostFXSource"),
+        fxSource2Id = Shader.PropertyToID("_PostFXSource2");
+
+    const int maxBloomPyramidLevels = 16;
+
+    int bloomPyramidId;
+
+    public PostFXStack()
+    {
+        // 只需记录第一个Id，因为unity是顺序分配的
+        bloomPyramidId = Shader.PropertyToID("_BloomPyramid0");
+        for (int i = 1; i < maxBloomPyramidLevels * 2; i++) { // 这里乘2是因为一次下采样中要执行两次滤波
+            Shader.PropertyToID("_BloomPyramid" + i);
+        }
+    }
 
     public void Setup(
         ScriptableRenderContext context, Camera camera, PostFXSettings settings
@@ -37,12 +63,11 @@ public partial class PostFXStack
 
     public void Render(int sourceId)
     {
-        Draw(sourceId, BuiltinRenderTextureType.CameraTarget, Pass.Copy); // simply copy
+        DoBloom(sourceId);
+        //Draw(sourceId, BuiltinRenderTextureType.CameraTarget, Pass.Copy); // simply copy
         context.ExecuteCommandBuffer(buffer);
         buffer.Clear();
     }
-
-    int fxSourceId = Shader.PropertyToID("_PostFXSource");
 
     void Draw(
         RenderTargetIdentifier from, RenderTargetIdentifier to, Pass pass
@@ -56,5 +81,88 @@ public partial class PostFXStack
             Matrix4x4.identity, settings.Material, (int)pass,
             MeshTopology.Triangles, 3
         );
+    }
+
+    void DoBloom(int sourceId)
+    {
+        buffer.BeginSample("Bloom");
+        var bloom = settings.Bloom;
+        int width = camera.pixelWidth / 2, height = camera.pixelHeight / 2;
+
+        if (
+            bloom.maxIterations == 0 || bloom.intensity <= 0f ||
+            height < bloom.downscaleLimit * 2 || width < bloom.downscaleLimit * 2
+        )
+        { // no bloom
+            Draw(sourceId, BuiltinRenderTextureType.CameraTarget, Pass.Copy);
+            buffer.EndSample("Bloom");
+            return;
+        }
+
+        Vector4 threshold;
+        threshold.x = Mathf.GammaToLinearSpace(bloom.threshold);
+        threshold.y = threshold.x * bloom.thresholdKnee;
+        threshold.z = 2f * threshold.y;
+        threshold.w = 0.25f / (threshold.y + 0.00001f);
+        threshold.y -= threshold.x;
+        buffer.SetGlobalVector(bloomThresholdId, threshold);
+
+        // 以半分辨率作为起点
+        RenderTextureFormat format = RenderTextureFormat.Default;
+        buffer.GetTemporaryRT(bloomPrefilterId, width, height, 0, FilterMode.Bilinear, format);
+        Draw(sourceId, bloomPrefilterId, Pass.BloomPrefilter);
+        width /= 2;
+        height /= 2;
+
+        int fromId = bloomPrefilterId, toId = bloomPyramidId + 1;
+        int i;
+        for (i = 0; i < bloom.maxIterations; i++)
+        {
+            if (height < bloom.downscaleLimit || width < bloom.downscaleLimit) break;
+            int midId = toId - 1;
+            buffer.GetTemporaryRT(midId, width, height, 0, FilterMode.Bilinear, format);
+            buffer.GetTemporaryRT(toId, width, height, 0, FilterMode.Bilinear, format);
+            // 高斯滤波加降采样
+            Draw(fromId, midId, Pass.BloomHorizontal);
+            Draw(midId, toId, Pass.BloomVertical);
+            fromId = toId;
+            toId += 2;
+            width /= 2;
+            height /= 2;
+        }
+
+        buffer.ReleaseTemporaryRT(bloomPrefilterId);
+
+        buffer.SetGlobalFloat(
+            bloomBicubicUpsamplingId, bloom.bicubicUpsampling ? 1f : 0f
+        );
+        buffer.SetGlobalFloat(bloomIntensityId, 1f);
+        if (i > 1)
+        {
+            buffer.ReleaseTemporaryRT(fromId - 1);
+            toId -= 5; // 使用倒数第二级的水平过滤结果作为辅助
+
+            // additive upsampling
+            for (i -= 1; i > 0; i--)
+            {
+                buffer.SetGlobalTexture(fxSource2Id, toId + 1);
+                Draw(fromId, toId, Pass.BloomCombine);
+
+                buffer.ReleaseTemporaryRT(fromId);
+                buffer.ReleaseTemporaryRT(toId + 1);
+                fromId = toId;
+                toId -= 2;
+            }
+        }
+        else
+        {
+            buffer.ReleaseTemporaryRT(bloomPyramidId);
+        }
+
+        buffer.SetGlobalFloat(bloomIntensityId, bloom.intensity);
+        buffer.SetGlobalTexture(fxSource2Id, sourceId);
+        Draw(fromId, BuiltinRenderTextureType.CameraTarget, Pass.BloomCombine);
+        buffer.ReleaseTemporaryRT(fromId);
+        buffer.EndSample("Bloom");
     }
 }
